83039c00c53041b7007e9dc2af0df7aa12325a50
Alec211 <alecalbr@iu.edu>
2024-02-25 19:58:04 -0500

    Update 0.1.1
    Same update to the sidebar. Now properly displays the placeholder for the user's name and their respective access level.

--- wellness-tracker-react/src/Components/Global/Sidebar.jsx
+++ wellness-tracker-react/src/Components/Global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  User's Name
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Access Level
                 </Typography>
               </Box>
             </Box>
