867423cdc729332e172b2ba07049666bee412499
Tim Chan <timchan@iu.edu>
2024-02-26 21:46:26 -0500

    merg alec

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import "./Dashboard.css";
-import Navbar from "../Navbar/Navbar";
+import { Box } from "@mui/material";
+import Header from "../Header";
+import NavbarHome from "../Navbar/NavbarHome";
 
 const Dashboard = () => {
-    return (
-        <div className="home">
-            <Navbar />
-            <div className="wrapper">
-                <h1>Dashboard</h1>
-                <main>
-                    <p>this is the dashboard/ homepage</p>
-                </main>
-            </div>
-        </div>
-    );
+  return (
+    <div className="home">
+      <NavbarHome />
+      <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="DASHBOARD" subtitle="Welcome to Instafit" />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Dashboard;
