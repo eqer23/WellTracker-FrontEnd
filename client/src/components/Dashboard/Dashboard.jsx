b2b585103be8331ace9cddddd55a37258839d8a8
Alec211 <alecalbr@iu.edu>
2024-02-28 17:21:08 -0500

    Moved in user.png asset

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "../Header";
-import NavbarHome from "../Navbar/NavbarHome";
+import Navbar from "../Navbar/Navbar";
 
 const Dashboard = () => {
   return (
     <div className="home">
-      <NavbarHome />
+      <Navbar />
       <Box m="20px">
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Header title="DASHBOARD" subtitle="Welcome to Instafit" />
