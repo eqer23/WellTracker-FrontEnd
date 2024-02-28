8f5755ce3daeddc7c20f6d046b7d11ece4f2b609
Alec211 <alecalbr@iu.edu>
2024-02-28 16:26:30 -0500

    Fixed import naming
    Because of file transfer some of the files were improperly pathed.

--- client/src/components/Team/Team.jsx
+++ client/src/components/Team/Team.jsx
@@ -7,7 +7,7 @@ import { mockDataTeam } from "../../Data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import Header from "../../Components/Header";
+import Header from "../../components/Header";
 
 const Team = () => {
   const theme = useTheme();
