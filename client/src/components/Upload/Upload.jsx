2485cf83fe74c5d69ac1dc3e223d683fa615ed8c
camillefraizer <camille.fraizer@gmail.com>
2024-03-20 15:09:27 -0400

    altered upload frontend to use formData object

--- client/src/components/Upload/Upload.jsx
+++ client/src/components/Upload/Upload.jsx
@@ -3,6 +3,8 @@ import "./Upload.css";
 import Navbar from "../Navbar/Navbar";
 import { Link, useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 let UPLOAD_URL = "http://localhost:3001/upload";
 
 
@@ -20,17 +22,19 @@ let UPLOAD_URL = "http://localhost:3001/upload";
 const Upload = () => {
 
     const [title, setTitle] = useState("");
-    const {token} = useParams()
+    const [file, setFile] = useState();
+    const {token} = useParams();
     const navigate = useNavigate();
 
 
     const handleSubmit = () => {
         if (title) {
+            const formData = new FormData()
+            formData.append('file', file)
+            formData.append('title', title)
             event.preventDefault();
             axios
-                .post("http://localhost:3001/upload/"+token, {
-                    title
-                })
+                .post("http://localhost:3001/upload/", formData)
                 .then((res) => {
                     if (res.data.status) {
                         console.log("Content uploaded");
@@ -38,6 +42,7 @@ const Upload = () => {
                     }
                     console.log(res.data)
                 })
+                .then()
                 .catch((err) => console.log(err));
         } else {
             alert("Please upload a file.");
@@ -53,17 +58,15 @@ const Upload = () => {
                     <p>Upload videos or workout plans here!</p>
 
 
-        {/* title input textbox */}
                 <div className="input-box">
-        <input
-            type="text"
-            placeholder="Enter Title Here"
-            required
-            onChange={(e) => setTitle(e.target.value)}
-            />
-        </div>  
-
-                    <button className="push-upload-btn" onClick={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Enter Title Here"
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div> 
+                <button className="push-upload-btn" onClick={handleSubmit}>
                     Submit
                 </button>
                 </main>
