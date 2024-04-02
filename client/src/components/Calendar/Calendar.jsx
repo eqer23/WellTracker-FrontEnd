e39ec540f655a0fe370e6ba32f3a2d681ba57454
sierranapieralski <111990838+sierranapieralski@users.noreply.github.com>
2024-04-02 11:15:30 -0400

    modified calendar

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     ScheduleComponent,
     ViewsDirective,
@@ -13,8 +13,10 @@ import {
     DragAndDrop,
 } from "@syncfusion/ej2-react-schedule";
 import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
-// import './Calendar.css';
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+let URL = import.meta.env.VITE_SERVER_URL;
 // import { scheduleData } from '../data/dummy';
 import Navbar from "../Navbar/Navbar";
 // eslint-disable-next-line react/destructuring-assignment
@@ -25,15 +27,111 @@ const Scheduler = ({
     availableViews = ["Day", "Week", "WorkWeek", "Month", "Agenda"],
 }) => {
     const [scheduleObj, setScheduleObj] = useState();
+    const [currentUser, setCurrentUser] = useState(undefined);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getFromBackend = async (id) => {
+            console.log("HI " + id);
+            try {
+                const response = await axios.get(URL + "sendevents", {
+                    params: {
+                        _userId: id,
+                    },
+                });
+                console.log(
+                    "Data successfully retreived from the backend:"
+                    // response.data
+                );
+                console.log(response);
+
+                return response;
+            } catch (error) {
+                console.error("Error sending data to backend:", error);
+            }
+        };
+        const fetchData = async () => {
+            if (!localStorage.getItem("session-token")) {
+                navigate("/login");
+                alert("You cannot use chat if you haven't logged in.");
+            } else {
+                const decodedToken = jwtDecode(
+                    localStorage.getItem("session-token")
+                );
+                setCurrentUser(decodedToken);
+                getFromBackend(decodedToken._id);
+            }
+        };
+
+        fetchData();
+    }, [navigate]);
 
     const change = (args) => {
         scheduleObj.selectedDate = args.value;
+        console.log(scheduleObj.selectedDate);
         scheduleObj.dataBind();
     };
 
     const onDragStart = (arg) => {
-        // eslint-disable-next-line no-param-reassign
         arg.navigation.enable = true;
+    };
+
+    const onActionComplete = (args) => {
+        console.log("in action complete: ", args);
+        // Checking if the action is either creating a new event or editing an existing one
+        if (
+            args.requestType === "eventCreated" ||
+            args.requestType === "eventChanged"
+        ) {
+            // args.data contains the event details in an array for create and as an object for edit
+            const eventData =
+                args.requestType === "eventCreate" ? args.data[0] : args.data;
+
+            console.log(eventData); // For debugging. You'll see this in your browser's console
+
+            // Send this data to your backend
+            console.log("doing things");
+            sendDataToBackend(eventData);
+        }
+    };
+
+    /*
+    Data Scehma -- make a new model that looks like user
+    { 
+        userId: 123
+        eventData: blah
+    }
+ 
+    > router
+    router.get(/frontendCallsBackend, backend())
+    router.get(/frontendCallsGetData, getData())
+
+    > controller 1 -- backend like login
+    function backend() {
+        const {currentUser, eventData} = req.body;
+        if (currentUser+_id in dataase {
+            do 
+        })
+    }
+
+    > controller 2 -- backend like chat
+    function getData() {
+        data.js/getAllUsers()
+    }
+    */
+
+    const sendDataToBackend = async (eventData) => {
+        try {
+            // axio.post(/frontendCallsGetdata)
+            const response = await axios.post(URL + "getevents", {
+                currentUser: currentUser._id,
+                eventData,
+            });
+
+            console.log("Data successfully sent to backend:", response.data);
+        } catch (error) {
+            console.error("Error sending data to backend:", error);
+        }
     };
 
     return (
@@ -46,15 +144,10 @@ const Scheduler = ({
                     selectedDate={new Date(2024, 3, 23)}
                     // eventSettings={{ dataSource: scheduleData }}
                     currentView={defaultView}
+                    currentUser={currentUser}
                     dragStart={onDragStart}
+                    actionComplete={onActionComplete}
                 >
-                    {/* <ViewsDirective>
-                        {["Day", "Week", "WorkWeek", "Month", "Agenda"].map(
-                            (item) => (
-                                <ViewDirective key={item} option={item} />
-                            )
-                        )}
-                    </ViewsDirective> */}
                     <ViewsDirective>
                         {availableViews.map(
                             (
@@ -82,7 +175,7 @@ const Scheduler = ({
                             <tr style={{ height: "50px" }}>
                                 <td style={{ width: "100%" }}>
                                     <DatePickerComponent
-                                        value={new Date(2024, 2, 23)}
+                                        value={new Date(0, 0, 0)}
                                         showClearButton={false}
                                         placeholder="Current Date"
                                         floatLabelType="Always"
@@ -99,36 +192,3 @@ const Scheduler = ({
 };
 
 export default Scheduler;
-
-// import * as React from "react";
-// import { createRoot } from "react-dom/client";
-// import {
-//     ScheduleComponent,
-//     Day,
-//     Week,
-//     WorkWeek,
-//     Month,
-//     Agenda,
-//     Inject,
-// } from "@syncfusion/ej2-react-schedule";
-
-// const Calendar = () => {
-//     return (
-//         <ScheduleComponent>
-//             <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-//         </ScheduleComponent>
-//     );
-// };
-
-// export default Calendar;
-
-// // Ensure the DOM element is available; this might vary depending on your HTML structure
-// document.addEventListener("DOMContentLoaded", () => {
-//     const container = document.getElementById("schedule");
-//     if (container) {
-//         const root = createRoot(container); // Use createRoot to create a root.
-//         root.render(<App />); // Use the render method on the created root.
-//     } else {
-//         console.error("Failed to find the target container");
-//     }
-// });
