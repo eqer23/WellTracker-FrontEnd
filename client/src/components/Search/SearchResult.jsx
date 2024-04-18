3467a5c2ca962d373c3877562b2a6797e96585f9
Isaac Navarrete <navarreteisaac14@gmail.com>
2024-04-18 17:14:45 -0400

    had to make search work again

--- client/src/components/Search/SearchResult.jsx
+++ client/src/components/Search/SearchResult.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "./SearchResult.css"
-export const SearchResult = ({result}) => {
-    return <div className="search-result" onClick={(e) => alert(`You clicked on ${result.name}`)}>{result.name}</div>;
+export const SearchResult = ({email}) => {
+    return <div className="search-result" onClick={(e) => alert(`You clicked on ${email}`)}>{email}</div>;
 }
